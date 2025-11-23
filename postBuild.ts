22e6ebd87106308237cabe55807485ab7a00d1eb
Peter Radko <cmtvpetya@gmail.com>
2025-11-23 16:14:36 +0300

    Types for common images/videos

--- postBuild.ts
+++ postBuild.ts
@@ -1,12 +1,21 @@
-import { readFileSync, writeFileSync } from 'node:fs';
+import { copyFileSync, readFileSync, writeFileSync } from 'node:fs';
 
 import pkg from './package.json' with { type: 'json' };
 
 const version = pkg.version;
 
-const singletonPath = './dist/singleton.js';
-let content = readFileSync(singletonPath, 'utf-8');
+export function insertVersion(singletonPath = './dist/singleton.js') {
+    let content = readFileSync(singletonPath, 'utf-8');
+    content = content.replace('{{ VERSION }}', version);
+    writeFileSync(singletonPath, content, 'utf-8');
+}
 
-content = content.replace('{{ VERSION }}', version);
+export function copyTypes(
+    source = './src/types.d.ts',
+    destination = './dist/types.d.ts',
+) {
+    copyFileSync(source, destination);
+}
 
-writeFileSync(singletonPath, content, 'utf-8');
+insertVersion();
+copyTypes();
