9c6e1803e4f1707eca0a5bd8577c9bbc7d466ff9
Peter Radko <cmtvpetya@gmail.com>
2025-12-07 15:16:00 +0300

    Disable throwing when singleton file is executed more than once

--- src/singleton.ts
+++ src/singleton.ts
@@ -22,13 +22,16 @@ export const PROSE_REGISTRY = PROSE_SINGLETON.registry;
 const singletonGlobalKey = '__JSPROSE__';
 
 if ((globalThis as any)[singletonGlobalKey]) {
-    throw new ProseError(
-        `
-Attempting to create multiple JSProse singleton instances in the same context!
-This can happen if multiple versions of @jsprose/core are included in the project.
-Also make sure not to inline @jsprose/core package in your project!
-        `.trim(),
-    );
+    // @TODO: Does not seem to work properly in SSR environments like Nuxt.
+    // Nitro execute this file, after that Nuxt App server side does it again in same context causing this error to be thrown.
+    //
+    //     throw new ProseError(
+    //         `
+    // Attempting to create multiple JSProse singleton instances in the same context!
+    // This can happen if multiple versions of @jsprose/core are included in the project.
+    // Also make sure not to inline @jsprose/core package in your project!
+    //         `.trim(),
+    //     );
 } else {
     (globalThis as any)[singletonGlobalKey] = PROSE_SINGLETON;
 }
